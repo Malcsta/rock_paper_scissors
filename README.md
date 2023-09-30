8096b5f55c39712a045c65b477f098595537304f
Malcolm White <mwhite2@rrc.ca>
2023-09-30 10:49:24 -0500

    Added slowly printing text and scoring system that resets when the game is exited.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This project is just a simple console based game ill use to 
-practice basic logic. I plan on adding a scoring feature in the
-future, and might add ASCII art to display images of rock, paper
-and scissors.
+practice basic logic. I have implemented a scoring feature, and
+in my spare time plan on working on the if statements to be less
+repetitive.
--- rps.py
+++ rps.py
@@ -12,19 +12,33 @@ will randomly make a choice.
 import random
 import os
 from time import sleep
+import time
+import sys
 
 user_selections = ('R', 'P', 'S')
 computer_selections = ('R', 'P', 'S')
 interface_active = True
+user_score = 0
+computer_score = 0
+
+def slowtype(message):
+    string_to_type = message
+    for char in string_to_type:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.15)
 
 
 ## Terminal window
 
 while interface_active:
     computer_selection = random.choice(computer_selections)
+    print()
     print("*"*40)
     print('{:^40s}'.format("ROCK PAPER SCISSORS")) 
     print('{:^40s}'.format("BY M@LC0LM"))
+    print('{:^40s}'.format(f"Your score: {user_score}"))
+    print('{:^40s}'.format(f"Computer score: {computer_score}"))
     print()
     print('{:^40s}'.format("Rock: R"))
     print('{:^40s}'.format("Paper: P"))
@@ -41,19 +55,13 @@ while interface_active:
         print('{:^40s}'.format("INVALID SELECTION"))
         print("*"*40)
         sleep(2)
-        os.system('clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
         continue
 
 
 ## Determining who wins
 
-    sleep(1)
-    print('3')
-    sleep(1)
-    print('2')
-    sleep(1)
-    print('1')
-    sleep(1)
+    slowtype('3..2..1..')
     print(f"You picked: {user_choice}")
     sleep(1)
     print(f"Computer picked: {computer_selection}")
@@ -65,51 +73,47 @@ while interface_active:
         print("*"*40)
 
     elif user_choice == 'S' and computer_selection == 'R':
-        print("*"*40)
+        
         print('{:^40s}'.format("Rock beats scissors!"))
         sleep(1)
-        print('{:^40s}'.format("Computer wins!")) 
-        print("*"*40)
-    
+        print('{:^40s}'.format("Computer wins!"))
+        computer_score += 1 
+
     elif user_choice == 'R' and computer_selection == 'P':
-        print("*"*40)
+        
         print('{:^40s}'.format("Paper beats rock!"))
         sleep(1)
-        print('{:^40s}'.format("Computer wins!")) 
-        print("*"*40)
-
+        print('{:^40s}'.format("Computer wins!"))
+        computer_score += 1  
+        
     elif user_choice == 'P' and computer_selection == 'S':
-        print("*"*40)
+        
         print('{:^40s}'.format("Scissors beats paper!")) 
         sleep(1)
-        print('{:^40s}'.format("Computer wins!")) 
-        print("*"*40)
-
-
+        print('{:^40s}'.format("Computer wins!"))
+        computer_score += 1  
+        
     elif user_choice == 'S' and computer_selection == 'P':
-        print("*"*40)
+        
         print('{:^40s}'.format("Scissors beats paper!")) 
         sleep(1)
         print('{:^40s}'.format("User wins!")) 
-        print("*"*40)
-
-
+        user_score += 1
+        
     elif user_choice == 'P' and computer_selection == 'R':
-        print("*"*40)
+        
         print('{:^40s}'.format("Paper beats rock!")) 
         sleep(1)
-        print('{:^40s}'.format("User wins!")) 
-        print("*"*40)
-
-
+        print('{:^40s}'.format("User wins!"))
+        user_score += 1 
+        
     elif user_choice == 'R' and computer_selection == 'S':
-        print("*"*40)
+        
         print('{:^40s}'.format("Rock beats Scissorss!")) 
         sleep(1)
-        print('{:^40s}'.format("User wins!")) 
-        print("*"*40)
-
-
+        print('{:^40s}'.format("User wins!"))
+        user_score += 1 
+        
 ## Asking if user wants to try again
 
     try_again = input('Try again? (Y/N)')
@@ -117,5 +121,5 @@ while interface_active:
     if try_again == 'Y':
         os.system('cls' if os.name == 'nt' else 'clear')
     elif try_again == 'N':
-        os.system('clear')
+        os.system('cls')
         interface_active = False
